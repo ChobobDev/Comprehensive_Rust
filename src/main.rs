6fe84a8689d4f19092fa786363035c0793d85a15
chobobdev <chobobdev@naver.com>
2023-02-28 15:25:31 +0900

    feat: Array assignment in rust

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,5 @@
 fn main() {
-    // Check if the commit shows up on github
-    let mut x: i32 = 14;
-    print!("{x}");
-    while x !=1{
-        if x%2==0{
-            x=x/2;
-
-        }else{
-            x=3*x+1;
-
-        }
-        print!(" -> {x}");
-    }
-    println!();
+    let mut a: [i8; 10] = [42; 10];
+    a[5] = 0;
+    println!("a:{:?}", a)
 }
